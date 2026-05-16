d3efd648a4b270014833544d163a697353a6ccef
GustavoCorrea10 <guganow11@gmail.com>
2026-05-15 22:40:12 -0300

    corrigi configurações de cors para github pages

--- src/main/java/com/educacao/assistente/config/CorsConfig.java
+++ src/main/java/com/educacao/assistente/config/CorsConfig.java
@@ -10,13 +10,31 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+
+                        .allowedOrigins(
+                                "http://127.0.0.1:5500",
+                                "http://localhost:5500",
+                                "https://gustavocorrea10.github.io"
+                        )
+
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        )
+
+                        .allowedHeaders("*")
+
+                        .allowCredentials(true);
             }
         };
     }
